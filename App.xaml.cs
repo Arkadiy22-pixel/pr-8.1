64b297d4e5119e080e72569e4eb497a64ab9e9e4
Тимофей Хохлов <magu_kf@mail.ru>
2025-05-30 09:57:16 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Интерфейс_ИС_Домовой
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
